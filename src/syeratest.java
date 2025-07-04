0aa13bb7bf208f0563fc15753dff95be52203b00
marxx002 <nafisahmaryam4@gmail.com>
2025-07-04 23:36:18 +0800

    Add new feature to ZooSystem

--- src/syeratest.java
+++ src/syeratest.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class syeratest {
+public class git fetch origin
+        git pull origin mainsyeratest {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -9,9 +10,7 @@ public class syeratest {
         System.out.printf("HAI INI DINI\n!");
         System.out.printf("HAI INI DINI\n!");
 
-
-        System.out.println("INI MARYAMMM");
-        System.out.println("INI MARYAMMM NAFISAHHH");
+        System.out.println("INI HOKKK BARUUUU");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
