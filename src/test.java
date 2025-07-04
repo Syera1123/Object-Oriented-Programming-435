2052e04c05d003249a325ae19eaabb1a9bcee777
Dhck03 <dinifarzana2903@gmail.com>
2025-07-05 00:22:37 +0800

    Add new feature to ZooSystem

--- src/test.java
+++ src/test.java
@@ -1,9 +1,7 @@
 public class test {
     public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("HELLO INI MARYAM NAFISAHH BARUU" );
 
 
-        System.out.println("Hello HII");
+        System.out.println("Hello HII DINI HAH");
     }
 }
