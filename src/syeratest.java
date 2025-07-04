1a1d6adfee8278081c81120b710dfdeb2876530a
marxx002 <nafisahmaryam4@gmail.com>
2025-07-04 23:04:01 +0800

    Add new feature to ZooSystem

--- src/syeratest.java
+++ src/syeratest.java
@@ -7,7 +7,7 @@ public class syeratest {
         System.out.printf("HAI INI SYERATEST\n!");
 
         System.out.println("INI MARYAMMM");
-
+        System.out.println("INI MARYAMMM NAFISAHHH");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
