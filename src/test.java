d3aa10b960543f6bfd2919e7644bcc9d175f5e7c
syeraazaliii <127415815+syeraazaliii@users.noreply.github.com>
2025-07-05 00:45:05 +0800

    Merge remote-tracking branch 'origin/Syera' into Syera

--- src/test.java
+++ src/test.java
@@ -1,8 +1,8 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("SYERA BESAQQQQQQQ NII");
-        System.out.println("HATI HATI DI JALAN RAYA");
+        System.out.println("MAKAN BESO");
+        System.out.println("HAHAHAAH LETIH NAA DAH NAK KUL 1");
 
     }
 }
