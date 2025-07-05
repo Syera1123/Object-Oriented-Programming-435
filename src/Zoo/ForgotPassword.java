a85bda9659af6056033a5a1a525d5354c117272c
syeraazaliii <127415815+syeraazaliii@users.noreply.github.com>
2025-07-06 06:34:36 +0800

    Add new feature to ZooSystem

---
+++ src/Zoo/ForgotPassword.java
@@ -0,0 +1,9 @@
+package Zoo;
+
+import javax.swing.*;
+
+public class ForgotPassword {
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+}
--- src/Zoo/WelcomePage.java
+++ src/Zoo/WelcomePage.java
@@ -5,33 +5,70 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public abstract class WelcomePage extends JFrame implements ActionListener,MouseListener {
-    public JPanel pnlMain;
+public abstract class WelcomePage extends JFrame implements ActionListener,MouseListener{
     public JButton btnSignUp;
     public JLabel lblTitle;
     public JLabel lblaccount;
     public JLabel lblLogIn;
+<<<<<<< HEAD
+    private JPanel welpanel;
+=======
+    public JPanel pnlMain;
+>>>>>>> origin/main
 
     public WelcomePage() {
 
         super.setTitle("Zoo Booking"); // title
-        super.setSize(400,300);
+<<<<<<< HEAD
+        super.setSize(700, 500);
+=======
+        super.setSize(700,500);
+>>>>>>> origin/main
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // === Background Image ===
+        JLabel background = new JLabel(new ImageIcon(
+                new ImageIcon(getClass().getResource("/Untitled design.png"))
+                        .getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH)
+        ));
+        background.setLayout(null); // Untuk absolute positioning
+        setContentPane(background);
+
+        pnlMain.setOpaque(false);
+        pnlMain.setBounds(0, 0, 700, 500);
+        background.add(pnlMain);
+
+
 
         //action Listener
         btnSignUp.addActionListener(this);
         lblLogIn.addMouseListener((MouseListener) this);
 
-        //add designed to Content Pane
-        Container cp = super.getContentPane();
-        cp.add(pnlMain);
+
 
         ImageIcon image = new ImageIcon("Zoo.jfif");
         super.setIconImage(image.getImage());
 
+        // === Background ===
+        JLabel backgroundLabel = new JLabel();
+        ImageIcon bgIcon = new ImageIcon(getClass().getResource("/Untitled design.png"));
+        backgroundLabel.setIcon(new ImageIcon(bgIcon.getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH)));
+        backgroundLabel.setBounds(0, 0, 700, 500);
 
+        welpanel.setOpaque(false);
 
+        // === Layering ===
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(700, 500));
 
+        backgroundLabel.setBounds(0, 0, 700, 500);
+        welpanel.setBounds(0, 0, 700, 500);
+
+        layeredPane.add(backgroundLabel, Integer.valueOf(0));
+        layeredPane.add(welpanel, Integer.valueOf(1));
+
+        setContentPane(layeredPane);
+        pack();
         super.setVisible(true);
 
     }
@@ -41,8 +78,33 @@ public abstract class WelcomePage extends JFrame implements ActionListener,Mouse
         new WelcomePage() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(e.getSource()==lblLogIn){
+                if (e.getSource() == lblLogIn) {
                     LogInWindow login = new LogInWindow() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mousePressed(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseReleased(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseEntered(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseExited(MouseEvent e) {
+
+                        }
+
                         @Override
                         public void actionPerformed(ActionEvent e) {
 
@@ -54,20 +116,24 @@ public abstract class WelcomePage extends JFrame implements ActionListener,Mouse
             }
 
             @Override
-            public void mousePressed(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {
+            }
 
             @Override
-            public void mouseReleased(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {
+            }
 
             @Override
-            public void mouseEntered(MouseEvent e) {}
+            public void mouseEntered(MouseEvent e) {
+            }
 
             @Override
-            public void mouseExited(MouseEvent e) {}
+            public void mouseExited(MouseEvent e) {
+            }
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(e.getSource()==btnSignUp){
+                if (e.getSource() == btnSignUp) {
                     new SignUpWindow() {
                         @Override
                         public void focusGained(FocusEvent e) {
